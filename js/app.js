472b33b3085bc015ab9845ded37bce3abce1abf4
Scott Schmidt <sjschmidt44@gmail.com>
2016-02-03 10:23:06 -0800

    Update repo to display example code for class 1 of this assignment.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-var data;
 var allProducts = [];
 var productNames = ['boots', 'chair', 'scissors', 'water_can', 'wine_glass', 'bag', 'banana', 'cthulhu', 'dragon', 'pen', 'shark', 'sweep', 'unicorn', 'usb'];
 
@@ -7,56 +6,28 @@ function Product(name, path) {
   this.path = path;
   this.tally = 0;
   this.views = 0;
-  data.labels.push(name);
-  data.datasets[0].data.push(0);
   allProducts.push(this);
 }
 
-function buildAlbum() {
+(function buildAlbum() {
   for (var i = 0; i < productNames.length; i++) {
     new Product(productNames[i], 'imgs/' + productNames[i] + '.jpg');
   }
-  localStorage.setItem('allProducts', JSON.stringify(allProducts));
-}
-
-(function checkLocal() {
-  if (localStorage.chartData && localStorage.allProducts) {
-    console.log('Exists');
-    data = JSON.parse(localStorage.chartData);
-    allProducts = JSON.parse(localStorage.getItem('allProducts'));
-  } else {
-    console.log('Doesnt exist');
-    data = {
-      labels: [],
-      datasets: [
-        {
-          label: "Product Analysis Results",
-          fillColor: "rgba(220,22,0,1)",
-          strokeColor: "rgba(220,22,0,0.8)",
-          highlightFill: "rgba(220,22,0,0.75)",
-          highlightStroke: "rgba(220,22,0,1)",
-          data: []
-        }
-      ]
-    };
-    buildAlbum();
-  }
-})();
+})()
 
 var productRank = {
   totalClicks: 0,
   leftObj: null,
   midObj: null,
   rightObj: null,
-  barChart: null,
 
   leftEl: document.getElementById('img1'),
   midEl: document.getElementById('img2'),
   rightEl: document.getElementById('img3'),
   imageEls: document.getElementById('images'),
+  resultsEl: document.getElementById('results'),
   resultsButton: document.getElementById('showResults'),
   resetButton: document.getElementById('reset'),
-  ctx: document.getElementById("canvas").getContext("2d"),
 
   getRandomIndex: function() {
     return Math.floor(Math.random() * productNames.length);
@@ -86,12 +57,21 @@ var productRank = {
       if (allProducts[i].name === elId) {
         allProducts[i].tally += 1;
         this.totalClicks += 1;
-        data.datasets[0].data[i] = allProducts[i].tally;
         console.log(allProducts[i].name + ' has ' + allProducts[i].tally + ' votes');
       }
     }
-    localStorage.setItem('chartData', JSON.stringify(data));
-    localStorage.setItem('allProducts', JSON.stringify(allProducts));
+  },
+
+  displayResults: function() {
+    var ulEl = document.createElement('ul');
+    for (var i in allProducts) {
+      var liEl = document.createElement('li');
+      var str = allProducts[i].name + ' has ' + allProducts[i].tally + ' votes.';
+      str = str.charAt(0).toUpperCase() + str.slice(1);
+      liEl.textContent = (str);
+      ulEl.appendChild(liEl);
+    }
+    this.resultsEl.appendChild(ulEl);
   },
 
   showButton: function() {
@@ -99,7 +79,8 @@ var productRank = {
     this.resultsButton.addEventListener('click', function() {
       productRank.resetButton.hidden = false;
       productRank.resultsButton.hidden = true;
-      productRank.barChart = new Chart(productRank.ctx).Bar(data);
+      productRank.displayResults();
+
       productRank.resetButton.addEventListener('click', function() {
         productRank.resetButton.hidden = true;
         location.reload();
